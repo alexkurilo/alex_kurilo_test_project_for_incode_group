918b423707fac267396bec4c496079d690a5ab0e
Alex <alex.kurilo77@gmail.com>
2018-10-30 23:45:21 +0200

    styles for client information

--- src/Containers/ClientsDetailsContainer.js
+++ src/Containers/ClientsDetailsContainer.js
@@ -1,24 +1,30 @@
 import React, { Component } from 'react';
 import {connect} from 'react-redux';
+import "./ClientsDetailsContainer.css";
 
 const ClientsDetailsContainer = ({findPersonData}) => {
-    const ShowSelectedpersonData = () => {
+    const ShowSelectedPersonData = () => {
         if (Object.keys(findPersonData).length !== 0) {
             return (
-                <div>
-                    <img    src={findPersonData.general.avatar}/>
-                    <section>{findPersonData.general.firstName+" "+findPersonData.general.lastName}</section>
-                    <section>company: {findPersonData.job.company}, title: {findPersonData.job.title}</section>
-                    <section>email: {findPersonData.contact.email}, phone: {findPersonData.contact.phone}</section>
-                    <section>address: {findPersonData.address.city+", "+findPersonData.address.country+", "+findPersonData.address.street}</section>
-                    <section>zipCode: {findPersonData.address.zipCode}</section>
+                <div className="findPersonInformation">
+                    <img    src={findPersonData.general.avatar}
+                            className="fullSizeAvatar"
+                    />
+                    <div className="detailPersonInformation">
+                        <section className="stringName">{findPersonData.general.firstName+" "+findPersonData.general.lastName}</section>
+                        <section className="stringJob"><em>title:</em> {findPersonData.job.title}, <em>company:</em> {findPersonData.job.company}</section>
+                        <section><em>email:</em> {findPersonData.contact.email}</section>
+                        <section><em>phone:</em> {findPersonData.contact.phone}</section>
+                        <section><em>address:</em> {findPersonData.address.city+", "+findPersonData.address.country+", "+findPersonData.address.street}</section>
+                        <section><em>zipCode:</em> {findPersonData.address.zipCode}</section>
+                    </div>
                 </div>
             );
         }
     };
     return(
-        <div>
-            {ShowSelectedpersonData(findPersonData)}
+        <div className="findPersonWrap">
+            {ShowSelectedPersonData(findPersonData)}
         </div>
     );
 };
